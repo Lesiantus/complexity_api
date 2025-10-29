146d54f806ffdb1e15e312e37f107e6728d73357
Lesiantus <sashkabonan@gmail.com>
2025-10-29 12:51:52 +0700

    deleted sleep, added retries

--- app/services/dictionary_client.rb
+++ app/services/dictionary_client.rb
@@ -10,7 +10,13 @@ class DictionaryClient
     return nil unless word.is_a?(String) && word.strip.present?
 
     url = URI("#{BASE_URL}/#{URI.encode_www_form_component(word)}")
-    response = Net::HTTP.get_response(url)
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.open_timeout = 5
+    http.read_timeout = 5
+
+    response = http.get(url)
 
     case response
     when Net::HTTPSuccess
@@ -21,13 +27,14 @@ class DictionaryClient
       nil
     when Net::HTTPTooManyRequests
       retry_after = response["Retry-After"]&.to_i || 5
-      {
-        rate_limited: true,
-        retry_after:
-      }
+      { rate_limited: true, retry_after: }
     else
-      raise "HTTP #{response.code}"
+      Rails.logger.error("DictionaryClient: HTTP #{response.code}")
+      nil
     end
+  rescue Net::OpenTimeout, Net::ReadTimeout => e
+    Rails.logger.warn("DictionaryClient timeout (#{word}): #{e.message}")
+    nil
   rescue JSON::ParserError => e
     Rails.logger.error("DictionaryClient JSON parse error (#{word}): #{e.message}")
     nil
--- app/sidekiq/complexity_score_worker_job.rb
+++ app/sidekiq/complexity_score_worker_job.rb
@@ -5,14 +5,14 @@ class ComplexityScoreWorkerJob
 
   BATCH_SIZE = 2
   DEFAULT_RETRY_DELAY = 5
+  MAX_FETCH_ATTEMPTS = 3
 
-  # Process job input in small batches and reschedule to enforce rate-limiting.
-  def perform(job_id, words = nil, accumulated = {})
+  def perform(job_id, words = nil, attempts_map = {})
     job = ComplexityJob.find(job_id)
     job.mark_in_progress! if words.nil?
 
     words ||= job.input
-    accumulated ||= {}
+    attempts_map ||= {}
 
     batch = words.take(BATCH_SIZE)
     remaining = words.drop(BATCH_SIZE)
@@ -20,40 +20,52 @@ class ComplexityScoreWorkerJob
     rate_limited = false
 
     batch.each do |word|
+      attempts_map[word] ||= 0
+      attempts_map[word] += 1
+
       data = DictionaryClient.fetch_definitions(word)
 
       if data.is_a?(Hash) && data[:rate_limited]
         delay = data[:retry_after] || DEFAULT_RETRY_DELAY
         to_retry = [word] + remaining
         Rails.logger.info("ComplexityScoreWorker: rate limited, rescheduling #{to_retry.size} word(s) in #{delay}s")
-        self.class.perform_in(delay, job_id, to_retry, accumulated)
+        self.class.perform_in(delay, job_id, to_retry, attempts_map)
         rate_limited = true
         break
       end
 
-      accumulated[word] = data ? compute_score(data) : nil
+      if data.nil? && attempts_map[word] < MAX_FETCH_ATTEMPTS
+        Rails.logger.warn("Retry #{attempts_map[word]}/#{MAX_FETCH_ATTEMPTS} for word=#{word}")
+        self.class.perform_in(0.5, job_id, [word], attempts_map)
+        next
+      end
+
+      score = data ? compute_score(data) : nil
+
+      job.with_lock do
+        job.result[word] = score
+        job.processed_count = job.result.size
+        job.save!
+      end
     end
 
     return if rate_limited
 
     if remaining.any?
-      self.class.perform_in(1, job_id, remaining, accumulated)
+      self.class.perform_in(1, job_id, remaining, attempts_map)
     else
-      job.mark_completed!(accumulated)
+      job.mark_completed!(job.result)
     end
   rescue StandardError => e
     job&.mark_failed!(e.message)
     Rails.logger.error("ComplexityScoreWorker failed: #{e.class} - #{e.message}")
   end
 
-  # Compute complexity score from parsed dictionary data.
   def compute_score(data)
     defs = data[:definitions_count].to_i
     syns = data[:synonyms_count].to_i
     ants = data[:antonyms_count].to_i
-
     return 0.0 if defs.zero?
-
     ((syns + ants).to_f / defs).round(2)
   end
 end
